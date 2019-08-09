53c734fe6707e78a6fb98da6a6157c1dd54d4b4b
Bartek Karelus <bartek.karelus@gmail.com>
2019-08-09 18:10:35 +0200

    add styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,30 @@
-console.log("działa")
+
 
 const btnAdd = document.querySelector(".button1_js")
 const btnDelete = document.querySelector(".button2_js")
 const h1 = document.querySelector(".h1")
 
-
+const key = new Date().toISOString().slice(0, 10);
+console.log(key)
 let counter = 0;
 h1.textContent = counter;
 const addGlass = () => {
     console.log("dodaj szklankę")
 
-    console.log(counter)
+
 
     h1.textContent = counter
     if (counter < 20) {
         ++counter
         h1.textContent = counter;
+        localStorage.setItem(key, h1.textContent)
     } else {
         counter = 20;
         alert("starczy Ci już")
     }
+    console.log(localStorage)
 }
+
 
 
 const deleteGlass = () => {
@@ -30,11 +34,14 @@ const deleteGlass = () => {
     if (counter > 0) {
         --counter
         h1.textContent = counter;
+        localStorage.setItem(key, h1.textContent)
     } else {
         counter = 0;
         h1.textContent = counter;
     }
+    console.log(localStorage)
 }
+
 
 
 btnAdd.addEventListener("click", addGlass)
